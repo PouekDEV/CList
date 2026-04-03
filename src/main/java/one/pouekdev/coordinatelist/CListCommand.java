7fc390651554e75c3a28c63e541c74ee4285cb52
xvrrv <brianfig90@gmail.com>
2026-04-03 04:00:59 -0300

    26.1/26.1.1

--- src/main/java/one/pouekdev/coordinatelist/CListCommand.java
+++ src/main/java/one/pouekdev/coordinatelist/CListCommand.java
@@ -2,7 +2,7 @@ package one.pouekdev.coordinatelist;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.commands.CommandBuildContext;
@@ -11,10 +11,10 @@ import org.jspecify.annotations.NonNull;
 public class CListCommand implements ClientCommandRegistrationCallback{
     @Override
     public void register(CommandDispatcher<FabricClientCommandSource> dispatcher, @NonNull CommandBuildContext buildContext){
-        dispatcher.register(ClientCommandManager.literal("clist")
-                .then(ClientCommandManager.argument("x", IntegerArgumentType.integer(Integer.MIN_VALUE, Integer.MAX_VALUE))
-                        .then(ClientCommandManager.argument("y", IntegerArgumentType.integer(Integer.MIN_VALUE, Integer.MAX_VALUE))
-                                .then(ClientCommandManager.argument("z", IntegerArgumentType.integer(Integer.MIN_VALUE, Integer.MAX_VALUE))
+        dispatcher.register(ClientCommands.literal("clist")
+                .then(ClientCommands.argument("x", IntegerArgumentType.integer(Integer.MIN_VALUE, Integer.MAX_VALUE))
+                        .then(ClientCommands.argument("y", IntegerArgumentType.integer(Integer.MIN_VALUE, Integer.MAX_VALUE))
+                                .then(ClientCommands.argument("z", IntegerArgumentType.integer(Integer.MIN_VALUE, Integer.MAX_VALUE))
                                         .executes(ctx -> {
                                             int x = IntegerArgumentType.getInteger(ctx, "x");
                                             int y = IntegerArgumentType.getInteger(ctx, "y");
