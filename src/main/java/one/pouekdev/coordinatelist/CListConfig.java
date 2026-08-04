16516184aa561e7c7fcc39a8a9bd7e8fb32f1044
Jakub Starostecki <jakub.starostecki27@gmail.com>
2026-08-04 20:16:58 +0200

    Tune the Regex for coords detection, Add #69

--- src/main/java/one/pouekdev/coordinatelist/CListConfig.java
+++ src/main/java/one/pouekdev/coordinatelist/CListConfig.java
@@ -9,4 +9,5 @@ public class CListConfig extends MidnightConfig {
     @Entry public static boolean canPlaceDeathpoints = true;
     @Entry public static boolean waypointTextBackground = true;
     @Entry public static boolean squareWaypoints = false;
+    @Entry public static boolean detectCoordsInChat = true;
 }
